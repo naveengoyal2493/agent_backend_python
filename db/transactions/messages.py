a3e4a6a57fceeccba07c708c976afdb36c19f775
Naveen Goyal <naveengoyal2493@gmail.com>
2021-09-10 23:25:02 +0530

    Addition : Added new constants for validating user

--- db/transactions/messages.py
+++ db/transactions/messages.py
@@ -1,3 +1,13 @@
 class Messages:
+
+    
     USER_NOT_FOUND = "No user exists with user_id as {user_id}"
     USER_ALREADY_EXISTS = "User with {user_id} already exists"
+    NAME_CANNOT_EXCEED_LIMIT = "Name cannot exceed {name_length} letters"
+    INVALID_GENDER = "The value provided for gender is invalid"
+    INVALID_DOB =  "{dob} is not a valid dob"
+    INVALID_EMAIL = "Not a valid email"
+    INVALID_FIELD_NAME = "The provided field name is invalid"
+    FIELD_NAME = "Field name = {field_name}"
+    USER_ID_CANNOT_BE_EMPTY = "User id cannot be empty"
+    USER_ID_IS_INCORRECT = "The given user id is incorrect"
