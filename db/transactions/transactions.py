6b6882de4f51f77f4341b6b4b20c34764ab2ce88
Naveen Goyal <naveengoyal2493@gmail.com>
2021-09-10 18:13:41 +0530

    Modification : Modified create_user method

--- db/transactions/transactions.py
+++ db/transactions/transactions.py
@@ -1,22 +1,39 @@
-from typing import NamedTuple
 from db.connection.db_conn import DbConnection
 from db.transactions.sql_statements import SQLStatements as sqls
-from db.transactions.exceptions import UserNotFoundException
+from db.transactions.constants import SQLConstants as sc
+from db.transactions.exceptions import UserNotFoundException, CreateUserException, UserAlreadyExistsException
 from db.transactions.messages import Messages
 from db.connection.constants import DbConstants as dc
-from utils.common import get_env_var
-from app_logging.log import Log
+import utils.common as cf
+
 
 class User:
 
+
     def __init__(self):
-        self.conn_instance = DbConnection(get_env_var(dc.ENV_DB_USERNAME), 
-                                        get_env_var(dc.ENV_DB_PASSWORD), 
-                                        get_env_var(dc.ENV_DB_HOSTNAME), 
-                                        get_env_var(dc.ENV_DB_NAME))
+        self.conn_instance = DbConnection(cf.get_env_var(dc.ENV_DB_USERNAME), 
+                                        cf.get_env_var(dc.ENV_DB_PASSWORD), 
+                                        cf.get_env_var(dc.ENV_DB_HOSTNAME), 
+                                        cf.get_env_var(dc.ENV_DB_NAME))
     
 
     def create_user(self, user_id, name=None, dob=None, gender=None, email=None):
+        if name and not cf.is_string_size_less_then_length(name, sc.NAME_LENGTH):
+            raise CreateUserException(f"Name cannot exceed {sc.NAME_LENGTH} letters")
+        
+        if gender and not cf.does_string_exist_in_list(gender, sc.GENDER_NOTATIONS):
+            raise CreateUserException("The value provided for gender is invalid")
+        
+        if dob and not cf.is_valid_date(dob, sc.DATE_FORMAT):
+            raise CreateUserException(f"{dob} is not a valid dob")
+        
+        if email:
+            if not (cf.is_string_size_less_then_length(email, sc.EMAIL_LENGTH) and cf.is_valid_email(email)):
+                raise CreateUserException("Not a valid email")
+        
+        if self.check_if_user_exists(user_id):
+            raise UserAlreadyExistsException(Messages.USER_ALREADY_EXISTS.format(user_id=user_id))
+
         self.conn_instance.execute_statement(
                         sqls.get_fmtsql_stmt(sqls.INSERT_USER, [user_id, name, dob, gender, email]))
 
