8c6f51d34f4476970c60ab82edb80ccd4762dd0c
Naveen Goyal <naveengoyal2493@gmail.com>
2021-09-10 23:24:06 +0530

    Addition : Added constants for policies

--- db/transactions/constants.py
+++ db/transactions/constants.py
@@ -7,7 +7,36 @@ class SQLConstants:
     USER_GENDER = "gender"
     USER_EMAIL = "email"
 
+    USER_ID_LENGTH = 28
     NAME_LENGTH = 50
     EMAIL_LENGTH = 50
     GENDER_NOTATIONS = ["M", "F", "O"]
     DATE_FORMAT = "%d-%m-%Y"
+
+    POLICY_TABLE = "policies"
+    POLICY_ID = "policy_id"
+    CUSTOMER_NAME = "customer_name"
+    CUSTOMER_MOBILE = "mobile"
+    CUSTOMER_EMAIL = "email"
+    CUSTOMER_RESIDENCE_ADDRESS = "residence_address"
+    CUSTOMER_DOB = "dob"
+    CUSTOMER_HEIGHT = "height"
+    CUSTOMER_WEIGHT = "weight"
+    CUSTOMER_OCCUPATION = "occupation"
+    POLICY_NO = "policy_no"
+    POLICY_START_DATE = "policy_start_date"
+    POLICY_MATURITY_DATE = "policy_maturity_date"
+    POLICY_PERIOD = "policy_period"
+    SUM_ASSURED = "sum_assured"
+    PREMIUM_MODE = "premium_mode"
+    PREMIUM_AMOUNT = "premium_amount"
+    PREMIUM_TERM = "term"
+    NOMINEE_NAME = "nominee_name"
+    NOMINEE_RELATION = "nominee_relation"
+    FATHER_FULL_NAME = "father_full_name"
+    MOTHER_FULL_NAME = "mother_full_name"
+    SPOUSE_NAME = "spouse_name"
+    POLICY_NAME = "policy_name"
+    POLICY_BOND_GIVEN = "policy_bond_given"
+    PREMIUM_RECEIPT_GIVEN = "premium_receipt_given"
+    OTHER_DETAILS = "other_details"
