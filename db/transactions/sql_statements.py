de24f69ab31b8f458832b78017fc4bf9e4a7f329
Naveen Goyal <naveengoyal2493@gmail.com>
2021-09-10 23:26:24 +0530

    Modification : better approach for SQLStatements

--- db/transactions/sql_statements.py
+++ db/transactions/sql_statements.py
@@ -14,7 +14,8 @@ class SQLStatements:
         return sql_stmt.format(rv=formatted_list)
 
 
-    def _update_user_query(update_value):
+    @staticmethod
+    def update_user(update_value):
         return f"Update {sc.USER_TABLE} SET " + update_value + "={rv[1]} where " + sc.USER_ID + " = {rv[0]}"
 
 
@@ -25,9 +26,5 @@ class SQLStatements:
     
     SELECT_USER = f"SELECT {sc.USER_NAME}, {sc.USER_EMAIL}, {sc.USER_DOB}, {sc.USER_GENDER} from {sc.USER_TABLE} where {sc.USER_ID} = " + "{rv[0]}"
 
-
-    UPDATE_USER_NAME = _update_user_query(sc.USER_NAME)
-    UPDATE_USER_DOB = _update_user_query(sc.USER_DOB)
-    UPDATE_USER_GENDER = _update_user_query(sc.USER_GENDER)
-    UPDATE_USER_EMAIL = _update_user_query(sc.USER_EMAIL)
-
+    INSERT_POLICY = f"INSERT INTO {sc.USER_TABLE} ({sc.USER_ID}, {sc.USER_NAME}, {sc.USER_DOB}, {sc.USER_GENDER}, \
+                                {sc.USER_EMAIL})" + " VALUES ({rv[0]}, {rv[1]}, STR_TO_DATE({rv[2]}, '%d-%m-%Y'), {rv[3]}, {rv[4]})"
